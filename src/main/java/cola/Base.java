f694c08b582492ab6a36178a8e12d5daa72bd2d2
dacastro42 <david.castro@talentum.edu.co>
2024-02-14 19:54:54 -0500

    Initial commit

---
+++ src/main/java/cola/Base.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package cola;
+
+/**
+ *
+ * @author pc
+ */
+public abstract class Base {
+    
+    public abstract Base copy();
+}
